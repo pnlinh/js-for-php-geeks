495f875038fe8b01a0c29738be1b00cf4f6e11f0
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-10-01 15:02:38 +0700

    post api

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -89,11 +89,16 @@
             e.preventDefault();
 
             var $form = $(e.currentTarget);
+            var formData = {};
+
+            $.each($form.serializeArray(), function (key, fieldData) {
+                formData[fieldData.name] = fieldData.value;
+            });
 
             $.ajax({
                 url: $form.data('url'),
                 method: 'POST',
-                data: $form.serialize(),
+                data: JSON.stringify(formData),
                 success: function (data) {
                     // todo
                     // $tbody.append(data);
